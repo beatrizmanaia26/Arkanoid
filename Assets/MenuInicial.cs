dd944519bd82a7c9766d9779f744e9242a7a9c66
beatriz <beatrizlberto@gmail.com>
2026-02-25 22:24:34 -0300

    inicio doprojeto 2 cenas iniciais

---
+++ Assets/MenuInicial.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+public class MenuInicial : MonoBehaviour
+{
+    // Método público para ser chamado pelo botão
+    public void Jogar()
+    {
+        Debug.Log("Botão clicado! Carregando cena...");
+        SceneManager.LoadScene("fase1");
+    }
+}
